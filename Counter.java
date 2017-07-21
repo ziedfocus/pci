dec8e6e0ce64a699f5601dc8f62952c48c6344eb
ZIED BEN SALEM <zied.salem.m@gmail.com>
2017-07-21 11:59:50 +0100

    wtf commit 2

--- Counter.java
+++ Counter.java
@@ -3,7 +3,7 @@ public class Counter{
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         for(int i =0;i<=10;i++)
-        System.out.println("Hello "+i);
+        System.out.println("ok Hello "+i);
     }
 
 }
